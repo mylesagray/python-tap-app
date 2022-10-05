3585a9520411bcf1bfa5511660473815accc2c02
Myles Gray <mg@mylesgray.com>
2022-10-05 14:29:45 +0100

    Adheres to autopep8 and pylint

--- app.py
+++ app.py
@@ -1,31 +1,40 @@
-from flask import Flask, request, render_template, jsonify
+"""Runs a simple Flask web app to print out a static web page"""
+from flask import Flask, render_template, jsonify
 import gunicorn
 
-app_name = "Python App With Flask, HTML, And REST"
-app = Flask(app_name)
-client = "VMware"
-framework = "Python with Pipenv"
+APP_NAME = "Python App With Flask, HTML, And REST"
+app = Flask(APP_NAME)
+CLIENT = "VMware"
+FRAMEWORK = "Python with Pipenv"
 
 messages = [
     {
-        'client':client,
-        'framework':framework,
-        'msg_subject':'Secure Software Supply Chains Are Great!', 
-        'msg_body':'With A Secure Software Supply Chain, Deploying Python Code To PROD is Safe, Reliable, And Fast!'
+        'client': CLIENT,
+        'framework': FRAMEWORK,
+        'msg_subject': 'Secure Software Supply Chains Are Great!',
+        'msg_body': 'With A Secure Software Supply Chain, \
+            Deploying Python Code To PROD is Safe, Reliable, And Fast!'
     }
 ]
 
+
 @app.route("/")
 def hello():
-    return render_template('index.html', client=client, framework=framework)
+    """Renders the web page template and serves"""
+    return render_template('index.html', client=CLIENT, framework=FRAMEWORK)
+
 
 @app.route('/messages')
 def get_incomes():
+    """Fakes a json API"""
     return jsonify(messages)
+
 
 @app.route('/versions')
 def versions():
+    """Prints out the current Gunicorn version"""
     gu_version = gunicorn.__version__
     return "Gunicorn version: " + gu_version
 
-app.debug=False
+
+app.debug = False
